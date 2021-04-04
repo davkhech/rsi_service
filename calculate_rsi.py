fe5e0746096680a0db6cbd6ecb518045bcfc0dd5
Davit Khechoyan <davkhech@gmail.com>
2021-04-04 13:17:30 +0400

    bar chart, correlation added

--- calculate_rsi.py
+++ calculate_rsi.py
@@ -1,5 +1,6 @@
 import argparse
 import pandas as pd
+import plotly.express as px
 
 from rsi import rsi
 
@@ -19,6 +20,8 @@ def parse_args():
                         type=str, help='end of the period to calculate RSI for, e.g. 2020-01-01')
     parser.add_argument('--output', '-o',
                         type=str, default='rsi.csv', help='rsi output csv file path')
+    parser.add_argument('--plot-trades', action='store_true', default=False, 
+                        help='show barchart of trades vs hour of the day')
     return parser.parse_args()
 
 
@@ -46,6 +49,13 @@ def main(args):
     rsi_df = rsi(close_prices, args.rsi_length)
     rsi_df.to_csv(args.output, header=['rsi'])
 
+    print(f'The correlation between the volume and RSI is {volumes.corr(rsi_df):.4}')
+    print(f'RSI is above 70 or below 30 in {((rsi_df > 70) | (rsi_df < 30)).mean() * 100:.4}% of times')
+
+    if args.plot_trades:
+        trades = df.groupby(df.index.hour)["number_of_trades"].sum()
+        fig = px.bar(trades, labels={'_id': 'hrs', 'value': 'trades'})
+        fig.show()
 
 if __name__ == "__main__":
     main(parse_args())
